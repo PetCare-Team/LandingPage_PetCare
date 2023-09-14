3c0edc7ba118bce0d6303aba4eef09b7a57f1836
EEduardoSanandres <102604575+EEduardoSanandres@users.noreply.github.com>
2023-09-14 11:37:44 -0500

    Update main.js
    Update main.js to final version

--- main.js
+++ main.js
@@ -13,6 +13,46 @@ function showRegistrationForm() {
     mainContent.classList.remove('login-content');
     mainContent.classList.remove('password-recovery-content');
     mainContent.classList.add('registration-content');
-  }
+}
   
 
+function showLoginForm() {
+    const mainContent = document.getElementById('main-content');
+    mainContent.innerHTML = `
+      <h2 class="welcome">Iniciar sesión</h2>
+      <input type="text" placeholder="Correo electrónico" class="input-field">
+      <input type="text" placeholder="Contraseña" class="input-field">
+      <a href="#" class="forgot-password" onclick="showPasswordRecoveryForm()">¿Olvidó su contraseña?</a>
+      <a href="#" class="register-link" onclick="showRegistrationForm()">¿Aún no posee una cuenta? Crear una.</a>
+      <button class="submit-btn">Iniciar sesión</button>
+    `;
+    mainContent.classList.remove('registration-content');
+    mainContent.classList.remove('password-recovery-content');
+    mainContent.classList.add('login-content');
+}
+  
+
+
+function showPasswordRecoveryForm() {
+    const mainContent = document.getElementById('main-content');
+    mainContent.innerHTML = `
+      <h2 class="welcome">Recuperar Contraseña</h2>
+      <p class="recovery-instructions">Escriba su correo electrónico para enviarle un código y pueda recuperar su contraseña</p>
+      <input type="text" placeholder="Correo electrónico" class="input-field" id="recovery-email" oninput="toggleSendButton()">
+      <button class="send-btn" id="send-btn" disabled>Enviar</button>
+    `;
+    mainContent.classList.add('password-recovery-content');
+}
+  
+function toggleSendButton() {
+    const emailInput = document.getElementById('recovery-email');
+    const sendButton = document.getElementById('send-btn');
+  
+    if (emailInput.value) {
+      sendButton.disabled = false;
+      sendButton.classList.add('send-btn-enabled');
+    } else {
+      sendButton.disabled = true;
+      sendButton.classList.remove('send-btn-enabled');
+    }
+}
